ba1b215a0aac2fa914b0bbb85a437f208852b13a
applecool <kmitharsha@gmail.com>
2015-02-23 01:43:26 -0600

    Added ProcessingDOM

---
+++ ProcessingDom/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace ProcessingDom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLanguage = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBoxUses = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBoxStandards = new System.Windows.Forms.ListBox();
+            this.chkdListProperties = new System.Windows.Forms.CheckedListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.lblError = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtStandards = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtUses = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Language :";
+            // 
+            // txtLanguage
+            // 
+            this.txtLanguage.Location = new System.Drawing.Point(94, 31);
+            this.txtLanguage.Name = "txtLanguage";
+            this.txtLanguage.Size = new System.Drawing.Size(138, 20);
+            this.txtLanguage.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "IntendedUses";
+            // 
+            // listBoxUses
+            // 
+            this.listBoxUses.FormattingEnabled = true;
+            this.listBoxUses.Location = new System.Drawing.Point(13, 114);
+            this.listBoxUses.Name = "listBoxUses";
+            this.listBoxUses.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBoxUses.Size = new System.Drawing.Size(154, 56);
+            this.listBoxUses.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Standard";
+            // 
+            // listBoxStandards
+            // 
+            this.listBoxStandards.FormattingEnabled = true;
+            this.listBoxStandards.Location = new System.Drawing.Point(12, 255);
+            this.listBoxStandards.Name = "listBoxStandards";
+            this.listBoxStandards.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBoxStandards.Size = new System.Drawing.Size(154, 56);
+            this.listBoxStandards.TabIndex = 2;
+            // 
+            // chkdListProperties
+            // 
+            this.chkdListProperties.CheckOnClick = true;
+            this.chkdListProperties.FormattingEnabled = true;
+            this.chkdListProperties.Items.AddRange(new object[] {
+            "Imperative",
+            "Object-Oriented",
+            "Functional",
+            "Procedural",
+            "Generic",
+            "Reflective",
+            "Event-Driven"});
+            this.chkdListProperties.Location = new System.Drawing.Point(83, 354);
+            this.chkdListProperties.MultiColumn = true;
+            this.chkdListProperties.Name = "chkdListProperties";
+            this.chkdListProperties.Size = new System.Drawing.Size(250, 64);
+            this.chkdListProperties.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 353);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Applies :";
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.Location = new System.Drawing.Point(149, 450);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(75, 23);
+            this.btn_Add.TabIndex = 4;
+            this.btn_Add.Text = "Add";
+            this.btn_Add.UseVisualStyleBackColor = true;
+            this.btn_Add.Click += new System.EventHandler(this.btn_Add_Click);
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(238, 31);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 13);
+            this.lblError.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(221, 80);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Other Uses :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 216);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Select Standards :";
+            // 
+            // txtStandards
+            // 
+            this.txtStandards.Location = new System.Drawing.Point(220, 255);
+            this.txtStandards.Multiline = true;
+            this.txtStandards.Name = "txtStandards";
+            this.txtStandards.Size = new System.Drawing.Size(115, 56);
+            this.txtStandards.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(183, 138);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(18, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Or";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(182, 275);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(18, 13);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Or";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(220, 216);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(90, 13);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "Other Standards :";
+            // 
+            // txtUses
+            // 
+            this.txtUses.Location = new System.Drawing.Point(221, 116);
+            this.txtUses.Multiline = true;
+            this.txtUses.Name = "txtUses";
+            this.txtUses.Size = new System.Drawing.Size(115, 56);
+            this.txtUses.TabIndex = 7;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(221, 93);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(142, 13);
+            this.label10.TabIndex = 8;
+            this.label10.Text = "(You can Add Multiple Items)";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(13, 80);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(67, 13);
+            this.label11.TabIndex = 0;
+            this.label11.Text = "Select Uses:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(217, 232);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(142, 13);
+            this.label12.TabIndex = 8;
+            this.label12.Text = "(You can Add Multiple Items)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 488);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtUses);
+            this.Controls.Add(this.txtStandards);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.btn_Add);
+            this.Controls.Add(this.chkdListProperties);
+            this.Controls.Add(this.listBoxStandards);
+            this.Controls.Add(this.listBoxUses);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtLanguage);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Add Languages";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLanguage;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBoxUses;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBoxStandards;
+        private System.Windows.Forms.CheckedListBox chkdListProperties;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtStandards;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtUses;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ ProcessingDom/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Collections;
+namespace ProcessingDom
+{
+    public partial class Form1 : Form
+    {
+        // path to the xml document
+        const string XML_FILE_PATH = "..\\..\\Resources\\Languages.xml";
+        public List<String> langList = new List<string>();
+        public Hashtable UsesMAP, StandardsMAP;
+     
+        public Form1()
+        {
+            InitializeComponent();
+            loadIntendedUses();
+            loadStandardTypes();
+            loadLanguages();
+
+        }
+
+        private void loadLanguages()
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(XML_FILE_PATH);
+
+            XmlElement root = xmlDoc.DocumentElement;
+            XmlNodeList languagesList = root.GetElementsByTagName("language");
+
+            for (int i = 0; i < languagesList.Count; i++)
+            {
+                langList.Add(languagesList.Item(i).FirstChild.InnerText);
+            }
+        }
+
+        private void loadIntendedUses()
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(XML_FILE_PATH);
+
+            XmlElement root = xmlDoc.DocumentElement;
+            XmlNode uses = root.LastChild.PreviousSibling;
+
+            XmlElement usesElement = (XmlElement)uses;
+            XmlNodeList usesList = usesElement.GetElementsByTagName("use");
+            UsesMAP = new Hashtable();
+            for (int i = 0; i < usesList.Count; i++)
+            {
+                listBoxUses.Items.Add(usesList.Item(i).InnerText);
+                UsesMAP.Add(usesList.Item(i).Attributes.GetNamedItem("uid").Value, usesList.Item(i).InnerText);
+            }
+
+        }
+
+        private void loadStandardTypes()
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(XML_FILE_PATH);
+
+            XmlElement root = xmlDoc.DocumentElement;
+            XmlNode standards = root.LastChild;
+
+            XmlElement standardsElement = (XmlElement)standards;
+            XmlNodeList standardsList = standardsElement.GetElementsByTagName("type");
+            StandardsMAP = new Hashtable();
+            for (int i = 0; i < standardsList.Count; i++)
+            {
+                listBoxStandards.Items.Add(standardsList.Item(i).InnerText);
+                StandardsMAP.Add(standardsList.Item(i).Attributes.GetNamedItem("tid").Value, standardsList.Item(i).InnerText);
+            }
+        }
+
+        private void btn_Add_Click(object sender, EventArgs e)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(XML_FILE_PATH); // load xml document from file system
+
+            XmlElement root = xmlDoc.DocumentElement;
+            // go to language element/node
+            XmlNode languagesNode = root.FirstChild;
+            XmlNode usesNode = root.LastChild.PreviousSibling;
+            XmlNode standardsNode = root.LastChild;
+            if (validateLanguageName())
+            {
+                XmlElement newLanguageElement = xmlDoc.CreateElement("language");
+                newLanguageElement.SetAttribute("id","_"+(root.ChildNodes.Count -1).ToString());
+
+                XmlElement newNameElement = xmlDoc.CreateElement("name");
+                newNameElement.InnerText = txtLanguage.Text;
+                XmlElement newImperativeElement, newObjectOrientedElement, newFunctionalElement, newProceduralElement, newGenericElement,
+                    newReflectiveElement, newEventDriven;
+                newImperativeElement = xmlDoc.CreateElement("imperative");
+                newObjectOrientedElement = xmlDoc.CreateElement("object-oriented");
+                newFunctionalElement = xmlDoc.CreateElement("functional");
+                newGenericElement = xmlDoc.CreateElement("generic");
+                newProceduralElement = xmlDoc.CreateElement("procedural");
+                newReflectiveElement = xmlDoc.CreateElement("reflective");
+                newEventDriven = xmlDoc.CreateElement("event-driven");
+                for (int i = 0; i < chkdListProperties.CheckedItems.Count; i++)
+                {
+                    if (chkdListProperties.CheckedItems.Contains("Imperative"))
+                    {
+                        newImperativeElement.InnerText = "Yes"; 
+                    }
+                    if (chkdListProperties.CheckedItems.Contains("Object-Oriented"))
+                    {
+                        newObjectOrientedElement.InnerText = "Yes";
+                    }
+                    if (chkdListProperties.CheckedItems.Contains("Functional"))
+                    {
+                        newFunctionalElement.InnerText = "Yes";
+                    }
+                    if (chkdListProperties.CheckedItems.Contains("Generic"))
+                    {
+                        newGenericElement.InnerText = "Yes"; 
+                    }
+                    if (chkdListProperties.CheckedItems.Contains("Procedural"))
+                    {
+                        newProceduralElement.InnerText = "Yes"; 
+                    }
+                    if (chkdListProperties.CheckedItems.Contains("Reflective"))
+                    {
+                        newReflectiveElement.InnerText = "Yes"; 
+                    }
+                    if (chkdListProperties.CheckedItems.Contains("Event-Driven"))
+                    {
+                        newEventDriven.InnerText = "Yes"; 
+                    }
+                }
+
+                String[] intededuses = txtUses.Text.Trim().Split('\n');
+
+                foreach (string use in intededuses)
+                {
+                    if (!UsesMAP.ContainsValue(use.TrimEnd('\r')))
+                    {
+                        XmlElement useElement = xmlDoc.CreateElement("use");
+                        useElement.SetAttribute("uid", "_" + (UsesMAP.Count + 1));
+                        useElement.InnerText = use.TrimEnd('\r');
+                        UsesMAP.Add("_" + (UsesMAP.Count + 1), use.TrimEnd('\r'));
+                        usesNode.AppendChild(useElement);
+                    }
+                    else if(use!="")
+                    {
+                        SetSelectedBreakType(listBoxUses, use.TrimEnd('\r'));
+                    }
+                    
+                }
+                String[] standardtypes = txtStandards.Text.Trim().Split('\n');
+
+                foreach (string type in standardtypes)
+                {
+                    if (!StandardsMAP.ContainsValue(type.TrimEnd('\r')))
+                    {
+                        XmlElement standardtype = xmlDoc.CreateElement("type");
+                        standardtype.SetAttribute("tid", "_0" + (StandardsMAP.Count + 1));
+                        standardtype.InnerText = type.TrimEnd('\r');
+                        StandardsMAP.Add("_0" + (StandardsMAP.Count + 1), type.TrimEnd('\r'));
+                        standardsNode.AppendChild(standardtype);
+                    }
+                    else if(type!="")
+                    {
+                        SetSelectedBreakType(listBoxStandards, type.TrimEnd('\r'));
+                    }
+                }
+                String useids="";
+
+                foreach (var item in listBoxUses.SelectedItems)
+                {
+                    foreach (DictionaryEntry use in UsesMAP)
+                    {
+                        if (item.ToString().Equals(use.Value))
+                        {
+                            useids += use.Key + " ";
+                        }
+                    }
+                }
+             
+                XmlElement IntendedUsesElement = xmlDoc.CreateElement("intendeduses");
+                XmlElement IntendedUseElement = xmlDoc.CreateElement("intendeduse");
+                IntendedUseElement.SetAttribute("id",useids.TrimEnd());
+             
+                String standardids = "";
+                foreach (var item in listBoxStandards.SelectedItems)
+                {
+                    foreach (DictionaryEntry standard in StandardsMAP)
+                    {
+                        if (item.ToString().Equals(standard.Value))
+                        {
+                            standardids += standard.Key + " ";
+                        }
+                    }
+                }
+                XmlElement standardtypesElement = xmlDoc.CreateElement("standardtypes");
+                XmlElement standardtypeElement = xmlDoc.CreateElement("standardtype");
+                standardtypeElement.SetAttribute("id", standardids.TrimEnd());
+
+                standardtypesElement.AppendChild(standardtypeElement);
+                IntendedUsesElement.AppendChild(IntendedUseElement);
+                // append title to language
+                newLanguageElement.AppendChild(newNameElement);
+                newLanguageElement.AppendChild(IntendedUsesElement);
+                newLanguageElement.AppendChild(newImperativeElement);
+                newLanguageElement.AppendChild(newObjectOrientedElement);
+                newLanguageElement.AppendChild(newFunctionalElement);
+                newLanguageElement.AppendChild(newProceduralElement);
+                newLanguageElement.AppendChild(newGenericElement);
+                newLanguageElement.AppendChild(newReflectiveElement);
+                newLanguageElement.AppendChild(newEventDriven);
+                newLanguageElement.AppendChild(standardtypesElement);
+                // append course to courses
+                root.PrependChild(newLanguageElement);
+
+                // save the changes
+                xmlDoc.Save(XML_FILE_PATH);
+
+                langList.Add(txtLanguage.Text);
+
+                txtLanguage.Text = "";
+                txtStandards.Text = "";
+                txtUses.Text = "";
+                listBoxStandards.ClearSelected();
+                listBoxUses.ClearSelected();
+                chkdListProperties.ClearSelected();
+                MessageBox.Show("Complete!");
+            }
+        }
+
+        private bool validateLanguageName()
+        {
+            if (txtLanguage.Text == "")
+            {
+                lblError.Text = "Please enter the Language Name";
+                return false;
+            }
+            if (langList.Contains(txtLanguage.Text))
+            {
+                lblError.Text = "Language Already Exists";
+                return false;
+            }
+
+            return true;
+        }
+        private void SetSelectedBreakType(ListBox lb, string value)
+        {
+            for (int i = 0; i < lb.Items.Count; i++)
+            {
+               
+                if (lb.Items[i].Equals(value))
+                {
+                    lb.SelectedIndices.Add(i);
+                    break;
+                }
+            }
+        }
+
+        
+    }
+}
